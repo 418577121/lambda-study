0098b5e8e01749654c57181d99918696a0967571
iwin <418577121@qq.com>
2021-08-14 12:05:37 +0800

    init

---
+++ lambda-02/src/com/dht/Test2.java
@@ -0,0 +1,11 @@
+package com.dht;
+
+/**
+ * @project_name: lambda-study
+ * @package_name: com.dht
+ * @description: 测试类2
+ * @author: DingHaiTing
+ * @create_time: 2021-08-14 12:04
+ **/
+public class Test2 {
+}
